f36b1b1f6b3140ebe00c448fbdeae785bd493170
Maryori B. <bautistachavezm@outlook.com>
2024-07-24 20:23:25 -0500

    Primer proyecto con Cucumber

---
+++ cypress/e2e/step_definition/login.js
@@ -0,0 +1,19 @@
+// cypress/e2e/duckduckgo.ts
+import { When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given("enter the principal page", () => {
+    //cy.visit("https://www.duckduckgo.com");
+    cy.log('Hola')
+});
+
+When("I visit duckduckgo.com", () => {
+    cy.visit("https://www.duckduckgo.com");
+});
+
+Then("I should see a search bar", () => {
+    cy.get("#searchbox_input").should(
+    "have.attr",
+    "placeholder",
+    "Search without being tracked"
+    );
+});
